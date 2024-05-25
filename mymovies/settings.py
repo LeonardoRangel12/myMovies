7f409165a9ad09deed2ac10060315f3a705f37bd
Leonardo Rangel Vizcarra <l20211830@tectijuana.edu.mx>
2024-05-25 04:17:25 +0000

    Fixed tailwind

--- mymovies/settings.py
+++ mymovies/settings.py
@@ -35,6 +35,9 @@ COMPRESS_ENABLED = True
  
 STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
 
+
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -135,6 +138,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
